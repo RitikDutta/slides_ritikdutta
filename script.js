6417c881e24c8d7240d5d63209d14acfc7beab2e
ritik dutta <ritikduttagd@gmail.com>
2026-04-29 09:43:34 +0530

    added new slide

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ const slideshowsProjects = {
     8: {
         title: "Aelum Consulting",
         slug: "aelum",
-        thumbnail: "https://raw.githubusercontent.com/RitikDutta/slides_ritikdutta/refs/heads/master/media/timestream.png",
+        thumbnail: "https://raw.githubusercontent.com/RitikDutta/slides_ritikdutta/refs/heads/master/media/ael.png",
         iframeSrc: "https://docs.google.com/presentation/d/e/2PACX-1vTznlqIz52_WwDaOeJJOCdnpnL6okh49g6Irb7bzH9CtJlMnOI8kvf0l-NYE7hXvGsaiCR8KP6Z9Ruo/pubembed?start=false&loop=false&delayms=3000"
     }
 };
